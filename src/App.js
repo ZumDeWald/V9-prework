76399ea0dfed3bfeb4ceeec13d084fb09e5938e4
ZumDeWald <zumdewald@gmail.com>
2019-05-20 21:46:33 -0500

    Footer has source code

--- src/App.js
+++ src/App.js
@@ -10,7 +10,15 @@ function App() {
 
       <section id='table' />
 
-      <footer id='footer' />
+      <footer id='footer' className='fbc'>
+        <a
+          href='https://github.com/ZumDeWald/V9-prework'
+          className='pointy'
+          target='_blank'
+          rel='noopener noreferrer'>
+          Source Code (github)
+        </a>
+      </footer>
     </div>
   );
 }
