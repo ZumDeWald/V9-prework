1838a983450a3ccad0cde1eba3e65bcefee68463
ZumDeWald <zumdewald@gmail.com>
2019-05-22 23:48:05 -0500

    Simplified search and next button

--- src/Table.js
+++ src/Table.js
@@ -20,13 +20,13 @@ function Table() {
   /* Handle Previous and Next buttons */
   const [range, setRange] = useState(0);
   const handlePrev = () => {
-    if (range >= 10) {
-      setRange(range - 10);
+    if (range >= 50) {
+      setRange(range - 50);
     }
   };
   const handleNext = () => {
-    if (range + 10 <= data.length) {
-      setRange(range + 10);
+    if (range + 50 <= viewData.length) {
+      setRange(range + 50);
     }
   };
 
@@ -35,29 +35,36 @@ function Table() {
   useEffect(() => {
     if (!!search) {
       const match = new RegExp(escapeRegExp(search), 'i');
-      setViewData(data.filter(entry => match.test(entry.name)).slice(0, 50));
-    } else if (!!data && range === 0) {
-      setViewData(data.slice(0, 10));
-    } else if (!!data && range > 0) {
-      setViewData(data.slice(range, range + 10));
+      setViewData(data.filter(entry => match.test(entry.name)));
+    } else if (!!data) {
+      setViewData(data);
     }
-  }, [data, search, range]);
+  }, [data, search]);
 
   const handleSetSearch = input => {
+    setRange(0);
     setSearch(input);
   };
 
   return (
     <main>
-      <input
-        className='search fbc'
-        type='text'
-        placeholder='Search By Name'
-        onChange={e => {
-          handleSetSearch(e.target.value);
-        }}
-        value={search}
-      />
+      <section className='search fbr'>
+        <input
+          className='fbc'
+          type='text'
+          placeholder='Search By Name'
+          onChange={e => {
+            handleSetSearch(e.target.value);
+          }}
+          value={search}
+        />
+        <button
+          onClick={() => {
+            setSearch('');
+          }}>
+          Clear Search
+        </button>
+      </section>
       <div id='table-container' className='pm0'>
         <ul id='table-header' className='fbr entry-container'>
           <li className='title-item'>Name</li>
@@ -67,21 +74,30 @@ function Table() {
           <li className='title-item'>Lat</li>
           <li className='title-item'>Long</li>
         </ul>
-        {!!viewData ? (
-          viewData.map(entry => <Entry entry={entry} key={entry.id} />)
+        {!!viewData && !!search ? (
+          viewData
+            .slice(range, range + 50)
+            .map(entry => <Entry entry={entry} key={entry.id} />)
+        ) : !!viewData ? (
+          viewData
+            .slice(range, range + 50)
+            .map(entry => <Entry entry={entry} key={entry.id} />)
         ) : (
           <div className='pm0 fbc'>No Matching Names</div>
         )}
       </div>
       <div className='prev-next'>
-        {range >= 10 && (
+        {range >= 50 && (
           <button name='prev' onClick={handlePrev}>
-            Previous 10
+            Previous 50
           </button>
         )}
-        <button name='next' onClick={handleNext}>
-          Next 10
-        </button>
+        {range < viewData.length && viewData.length > 50 && (
+          <button name='next' onClick={handleNext}>
+            Next 50
+          </button>
+        )}
+        <p>Total Results: {viewData.length}</p>
       </div>
     </main>
   );
